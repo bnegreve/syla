2f8b06ea6fcad6de82d1553a9ae77c94b8d5b5ad
Benjamin Negrevergne <bnegreve@gmail.com>
2018-10-12 20:04:12 +0200

    Various changes again

--- Coinche/Ai/Tools.hs
+++ Coinche/Ai/Tools.hs
@@ -8,69 +8,10 @@ import Control.Lens
 import Coinche.Engine
 import System.Random.Shuffle
 import System.Random
-
+import Data.Maybe
 import Data.List
 import qualified Data.Array as A
-
-
-{- create a partition of l where each member of the partition is of size n-}
-group' :: Int -> [a] -> [[a]]
-group' _ [] = []
-group' n l
-  | n > 0 = (take n l) : (group' n (drop n l))
-    | otherwise = error "Negative n"
-
-terminated g =
-  and $ null . view _w <$> A.elems (_gPlayersHands g) 
-  
-distribuerCartes = do
-  cards <- shuffleCards
-  pure $ A.listArray (P_1,P_4) $ Hand . sortCards <$>  group' 8 cards
-
-gagnantPli :: Atout -> Pli -> Player
-gagnantPli atout pli = winner
-  where (card, winner) = foldl1 f (view _w pli) -- La carte initiale determine la couleur du pli
-        f acc el = case comparerCarte atout (fst acc) (fst el) of
-                     GT -> acc
-                     LT -> el
-                     EQ -> undefined
-jouerCarte' :: Atout -> Game -> Card -> Game
-jouerCarte' atout g c = gFinal
-  where g' = g & gJoueursRestants %~ tail
-        g'' = g' & gPliCourant . _w %~ (++ [(c,curPlayer)])
-        g''' = g'' & gPlayersHands . ix curPlayer . _w %~ delete c
-        gagnant = gagnantPli atout (_gPliCourant g''')
-        gFinal = if null $ _gJoueursRestants g''' then
-                 g'''{_gPlisJoues = _gPlisJoues g''' ++ [(_gPliCourant g''',gagnant)],
-                     _gPliCourant = Pli [],
-                     _gJoueursRestants = take 4 $ dropWhile (/= gagnant) $ cycle [P_1 .. P_4] }
-               else
-                 g'''
-        curPlayer = head $ _gJoueursRestants g
-
-coupsPossibles' :: Atout -> Game -> [Card]
-coupsPossibles' atout g = validMoves atout g curHand  --coupsPossibles atout (RoundColor roundColor) False (fst <$> pliCourant) curHand
-  where --pliCourant = view _w $ _gPliCourant g
-        curPlayer =  head $ _gJoueursRestants g
-        curHand = _gPlayersHands g A.! curPlayer 
-        --roundColor = _cColor $ head $ fst <$> pliCourant        
-validMoves :: Atout -> Game -> Hand -> [Card]
-validMoves atout g hand = coupsPossibles atout (RoundColor roundColor) False (fst <$> pliCourant) hand 
-  where pliCourant = view _w $ _gPliCourant g
-        roundColor = _cColor $ head $ fst <$> pliCourant        
-
-
-score :: Player -> Atout -> Game -> Int
-score moi atout g
-  | lastWinner == moi || lastWinner == coequipier moi = compteDesPoints + 10
-  | otherwise = compteDesPoints
-  where plisJoues = _gPlisJoues g
-        valeurs = [(valeurPli atout (fmap fst pli), gagnant) | (Pli pli,gagnant) <- plisJoues] 
-        compteDesPoints = foldl f 0 valeurs
-        f acc (val, gagnant)
-          | gagnant == moi || coequipier gagnant == moi = acc + val
-          | otherwise = acc
-        lastWinner = snd $ last plisJoues 
+import qualified Data.Map as M
 
 -- Plays a random game and returns the score from player's perspective
 rollout :: Player -> Atout -> Game -> IO Double
@@ -87,3 +28,127 @@ roll terminalP coups jouer s
   |otherwise = do let possibilites = coups s
                   randomCoup <- head <$> shuffleM possibilites
                   roll terminalP coups jouer (jouer s randomCoup) 
+
+-- simply returns the best move in a list of tuples (moves, scores)
+bestmove :: [(m, Double)] -> m
+bestmove movescores =
+  let max = maximumBy (\(m,s) (m',s') -> s `compare` s') movescores in
+    fst max
+
+-- Runs n rollouts and returns the average
+-- score for each card.
+simul :: Atout -> Int -> Game -> Player -> [Card] -> IO [(Card, Double)]
+simul atout n game player cards = do
+  forM (zip [1..] cards) $ \(i,c) -> do
+    let m' = if r /= 0 && rem i (div l r) == 0 then m+1 else m -- redistribute remaining simus
+    cardscores <- forM [1..m'] $ \_ -> do
+      rollout player atout (jouerCarte' atout game c) 
+    let cardavgscore = sum cardscores / fromIntegral m'
+    pure (c, cardavgscore)
+  where l = length cards
+        m = div n l --round $ (fromIntegral n) / (fromIntegral (length cards))
+        r = rem n l
+
+-- Returns the cards that have not been seen by player
+-- (cards that have not been played and are not in player's hand)
+-- shuffles the output the the ai cannot learn anything from the order.
+remainingCards :: Game -> Player -> IO [Card]
+remainingCards g p = do
+  shuffleM $ concat [mi | (pi,Hand mi) <- A.assocs $ _gPlayersHands g, pi /= p]
+
+-- Build random hand for each player given a set of remaining cards
+-- and a hand size of each player
+-- Warning: remainingCards should be randomized before 
+buildRandomHands :: Game -> [Card] -> [(Player,Int)] -> [(Player,Hand)]
+buildRandomHands game remainingCards sizes = fst $ foldr f ([],remainingCards) sizes
+  where f (player,size) (hands,remCards) = let (hand, rest) = splitAt size remCards
+                                  in ((player,Hand hand) :hands, rest)
+{- fonction violente -}
+buildRandomHands' :: Game
+                  -> [Card] -- remaining cards
+                  -> [(Player, Int)] -- list of hand sizes for each player
+                  -> Maybe [(Player, Hand)] -- list of player hands
+buildRandomHands' game remcards sizes
+  | isNothing result = Nothing
+  |otherwise = Just [(pi,hi) | (pi, hi@(Hand h)) <- A.assocs $ fst $ fromJust result,
+                                                  not $ null h ]
+               
+  where players = fst <$> sizes
+        initialHands :: A.Array Player Hand
+        initialHands = A.listArray (P_1,P_4) $ take 4 (repeat $ Hand [])
+        initialSizes = A.listArray (P_1,P_4) (take 4 (repeat 0)) A.// sizes
+        result = foldl f (Just (initialHands, initialSizes)) remcards
+        f Nothing _ = Nothing
+        f (Just (hands, remsizes)) card =
+          let playercandidates = [ pi | -- pi <- players,
+                                       pi <- fst <$> sortBy (\(pi,cs) (pi', cs') -> length cs `compare` length cs') (M.toList playersPossibleColors),
+                                      remsizes A.! pi > 0, -- player  hand is not full
+                                      playerCanHaveColor game pi (_cColor card)]
+              fstplayer = head playercandidates
+              fstplayerhand = view _w $ hands A.! fstplayer
+              playersPossibleColors :: M.Map Player [Color]
+              playersPossibleColors = M.fromList [(pi,possibleColors) |
+                                                   pi <- players,
+                                                   let possibleColors = [color | color <- colors, playerCanHaveColor game pi color]]
+          in case playercandidates of
+            [] -> Nothing
+            otherwise -> Just (hands A.// [(fstplayer, Hand $ card:fstplayerhand)],
+                               remsizes A.// [(fstplayer, remsizes A.! fstplayer - 1)] )
+
+
+-- -- Build a player hands that is consistant with his/her previous actions 
+playerPossibleCards :: Game -> Player -> [Card] -> [Card]
+playerPossibleCards game player remcards = [ci | ci <- remcards,
+                                            _cColor ci `elem` possiblecolors]
+ where prevturns = _gPlisJoues game
+       possiblecolors = [color | color <- colors,
+                                 playerCanHaveColor game player color]
+
+-- Returns True if player has always followed with the color
+playerCanHaveColor :: Game -> Player -> Color -> Bool
+playerCanHaveColor game player color = and $ playerPlayedColor player <$> turnscolor
+  where turns = (_gPliCourant game) : (fst <$> _gPlisJoues game)
+        turnscolor = [ti | ti <- turns,
+                           isJust $ turnColor ti,
+                           fromJust (turnColor ti) == color]
+
+-- Returns True if player has played color in turn 
+playerPlayedColor :: Player -> Pli -> Bool 
+playerPlayedColor player t@(Pli turn)
+  | isNothing turncolor = True
+  | null playercards = True
+  | otherwise = _cColor (head playercards) == fromJust turncolor
+  where turncolor = turnColor t
+        playercards = [ci | (ci,pi) <- turn, pi == player]
+
+-- Returns the partially observed game from player's perspective
+partiallyObservedGame :: Player -> Game -> Game
+partiallyObservedGame p g = 
+  g { _gPlayersHands =
+        _gPlayersHands g A.// [(pi, Hand []) | pi <- [P_1 .. P_4], pi /= p]  }
+
+-- Sample a possible game that is compatible with the current player game
+-- Warning: playergame must be a partially observed game (obtained with playerGame)
+samplePossibleGame :: Game -> Player -> [Card] -> IO Game
+samplePossibleGame g p remcards =
+  pure $ g & gPlayersHands %~ (A.// randomhands)
+  where phandsize = length $ view _w $ _gPlayersHands g A.! p
+        mustPlay = _gJoueursRestants g \\ [p]
+        havePlayed =  (snd <$> (g ^. gPliCourant . _w)) \\ [p]
+        randomhands = buildRandomHands g remcards $ 
+                      [(pi,phandsize) | pi <- mustPlay]
+                      ++ [(pi,phandsize - 1) | pi <- havePlayed]
+
+-- Sample a possible game that is compatible with the current player game
+-- Warning: playergame must be a partially observed game (obtained with playerGame)
+samplePossibleGameSmart :: Game -> Player -> [Card] -> IO (Maybe Game)
+samplePossibleGameSmart g p remcards
+  | isNothing randomhands = pure Nothing
+  | otherwise = pure $ Just $ g & gPlayersHands %~ (A.// fromJust randomhands)
+  where phandsize = length $ view _w $ _gPlayersHands g A.! p
+        mustPlay = _gJoueursRestants g \\ [p]
+        havePlayed =  (snd <$> (g ^. gPliCourant . _w)) \\ [p]
+        randomhands = buildRandomHands' g remcards $ 
+                      [(pi,phandsize) | pi <- mustPlay]
+                      ++ [(pi,phandsize - 1) | pi <- havePlayed]
+
--- Coinche/Engine/Game.hs
+++ Coinche/Engine/Game.hs
@@ -91,3 +91,68 @@ compterPlis atout = do
         undefined    
 
 
+
+{- create a partition of l where each member of the partition is of size n-}
+group' :: Int -> [a] -> [[a]]
+group' _ [] = []
+group' n l
+  | n > 0 = (take n l) : (group' n (drop n l))
+    | otherwise = error "Negative n"
+
+terminated g =
+  and $ null . view _w <$> A.elems (_gPlayersHands g) 
+  
+distribuerCartes = do
+  cards <- shuffleCards
+  pure $ A.listArray (P_1,P_4) $ Hand . sortCards <$>  group' 8 cards
+
+gagnantPli :: Atout -> Pli -> Player
+gagnantPli atout pli = winner
+  where (card, winner) = foldl1 f (view _w pli) -- La carte initiale determine la couleur du pli
+        f acc el = case comparerCarte atout (fst acc) (fst el) of
+                     GT -> acc
+                     LT -> el
+                     EQ -> undefined
+jouerCarte' :: Atout -> Game -> Card -> Game
+jouerCarte' atout g c = gFinal
+  where g' = g & gJoueursRestants %~ tail
+        g'' = g' & gPliCourant . _w %~ (++ [(c,curPlayer)])
+        g''' = g'' & gPlayersHands . ix curPlayer . _w %~ delete c
+        gagnant = gagnantPli atout (_gPliCourant g''')
+        gFinal = if null $ _gJoueursRestants g''' then
+                 g'''{_gPlisJoues = _gPlisJoues g''' ++ [(_gPliCourant g''',gagnant)],
+                     _gPliCourant = Pli [],
+                     _gJoueursRestants = take 4 $ dropWhile (/= gagnant) $ cycle [P_1 .. P_4] }
+               else
+                 g'''
+        curPlayer = head $ _gJoueursRestants g
+
+coupsPossibles' :: Atout -> Game -> [Card]
+coupsPossibles' atout g = validMoves atout g curHand  --coupsPossibles atout (RoundColor roundColor) False (fst <$> pliCourant) curHand
+  where --pliCourant = view _w $ _gPliCourant g
+        curPlayer =  head $ _gJoueursRestants g
+        curHand = _gPlayersHands g A.! curPlayer 
+        --roundColor = _cColor $ head $ fst <$> pliCourant        
+validMoves :: Atout -> Game -> Hand -> [Card]
+validMoves atout g hand = coupsPossibles atout (RoundColor roundColor) False (fst <$> pliCourant) hand 
+  where pliCourant = view _w $ _gPliCourant g
+        roundColor = _cColor $ head $ fst <$> pliCourant        
+
+
+score :: Player -> Atout -> Game -> Int
+score moi atout g
+  | lastWinner == moi || lastWinner == coequipier moi = compteDesPoints + 10
+  | otherwise = compteDesPoints
+  where plisJoues = _gPlisJoues g
+        valeurs = [(valeurPli atout (fmap fst pli), gagnant) | (Pli pli,gagnant) <- plisJoues] 
+        compteDesPoints = foldl f 0 valeurs
+        f acc (val, gagnant)
+          | gagnant == moi || coequipier gagnant == moi = acc + val
+          | otherwise = acc
+        lastWinner = snd $ last plisJoues 
+
+-- Return the color of a particular turn or Nothing if turn is empty so far
+turnColor :: Pli -> Maybe Color
+turnColor (Pli []) = Nothing
+turnColor (Pli turn) = Just $ _cColor $ fst $ head turn
+  
